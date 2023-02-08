95355b4c18f675e5c3d5e92f456cea46380a2908
spencerrfree <sf376@byu.edu>
2023-02-08 13:03:59 -0700

    up to date

---
+++ Sf376_Mission6/Models/ApplicationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sf376_Mission6.Models
+{
+    public class ApplicationResponse
+    {
+        public string Rating { get; set; }
+    }
+}
